54499330779d1dcda7654e869a2d5f1087201377
i-neeed-sleep <kammyl@nycstudents.net>
2025-12-11 14:01:30 -0500

    eeeeeeeeeeeeeeeee

--- dnd/src/main.js
+++ dnd/src/main.js
@@ -1,4 +1,17 @@
 
-const api = 'https://api.open5e.com/v2/conditions/';
+async function getData(){
+    try{
+        const response = await fetch(`https://api.open5e.com/v1/races/`);
 
+        if (response.status != 200){
+            throw new Error(response);
+        }else {
+            const data = await response.json();
+            document.getElementById("api-response").textContent = data.name;
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
 
+getData('alseid');
