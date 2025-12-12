20fa4e21f2e819820367d8fb6be87dd5e473611d
i-neeed-sleep <kammyl@nycstudents.net>
2025-12-12 14:02:10 -0500

    eeeeeeeeeeeeeeeee

--- dnd/src/main.js
+++ dnd/src/main.js
@@ -1,4 +1,17 @@
 
+let names = [];
+let descs = [];
+let asis = [];
+let ages = [];
+let alignments = [];
+let sizes = [];
+let speeds = [];
+let languagess = [];
+let visions = [];
+let traitss = [];
+let document = [];
+
+
 async function getData(){
     try{
         const response = await fetch(`https://api.open5e.com/v1/races/`);
@@ -7,11 +20,36 @@ async function getData(){
             throw new Error(response);
         }else {
             const data = await response.json();
-            document.getElementById("api-response").textContent = data.name;
+            data.results.forEach(i => {
+                names.push(i.name);
+                descs.push(i.desc);
+                asis.push(i.asi_desc);
+                ages.push(i.age);
+                alignments.push(i.alignment);
+                sizes.push(i.size);
+                speeds.push(i.speed_desc);
+                languagess.push(i.languages);
+                document.push(i.document_title);
+
+                if (i.vision === ""){
+                    visions.push("This race has regular vision.");
+                } else {
+                    visions.push(i.vision);
+                }
+                
+                if (i.traits === ""){
+                    traitss.push("This race has no special traits");
+                } else {
+                    traitss.push(i.traits);
+                }
+            });
         }
     } catch(error) {
         console.log(error);
     }
 }
 
-getData('alseid');
+getData();
+
+
+console.log(names[1]);
