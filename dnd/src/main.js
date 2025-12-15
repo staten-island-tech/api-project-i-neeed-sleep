116d0dffe6c3c88ccdc359ba926add85848f36c5
i-neeed-sleep <kammyl@nycstudents.net>
2025-12-15 14:02:15 -0500

    eeeeeeeeeee

--- dnd/src/main.js
+++ dnd/src/main.js
@@ -11,45 +11,44 @@ let visions = [];
 let traitss = [];
 let document = [];
 
-
 async function getData(){
     try{
         const response = await fetch(`https://api.open5e.com/v1/races/`);
 
         if (response.status != 200){
             throw new Error(response);
-        }else {
+        } else {
             const data = await response.json();
-            data.results.forEach(i => {
-                names.push(i.name);
-                descs.push(i.desc);
-                asis.push(i.asi_desc);
-                ages.push(i.age);
-                alignments.push(i.alignment);
-                sizes.push(i.size);
-                speeds.push(i.speed_desc);
-                languagess.push(i.languages);
-                document.push(i.document_title);
-
-                if (i.vision === ""){
-                    visions.push("This race has regular vision.");
-                } else {
-                    visions.push(i.vision);
-                }
-                
-                if (i.traits === ""){
-                    traitss.push("This race has no special traits");
-                } else {
-                    traitss.push(i.traits);
-                }
-            });
+            console.log(data.results);
         }
     } catch(error) {
         console.log(error);
-    }
-}
+}}
 
 getData();
 
+/*
 
-console.log(names[1]);
+data.results.forEach(i => {
+                    names.push(i.name);
+                    descs.push(i.desc);
+                    asis.push(i.asi_desc);
+                    ages.push(i.age);
+                    alignments.push(i.alignment);
+                    sizes.push(i.size);
+                    speeds.push(i.speed_desc);
+                    languagess.push(i.languages);
+                    document.push(i.document_title);
+
+                    if (i.vision === ""){
+                        visions.push("This race has regular vision.");
+                    } else {
+                        visions.push(i.vision);
+                    }
+                    
+                    if (i.traits === ""){
+                        traitss.push("This race has no special traits");
+                    } else {
+                        traitss.push(i.traits);
+                    }
+                }); */
