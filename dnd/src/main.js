b097195ddd8657686a14feec1914343ebdffff1a
i-neeed-sleep <kammyl@nycstudents.net>
2025-12-16 13:05:23 -0500

    eeeeeeeeee

--- dnd/src/main.js
+++ dnd/src/main.js
@@ -1,54 +1,26 @@
 
-let names = [];
-let descs = [];
-let asis = [];
-let ages = [];
-let alignments = [];
-let sizes = [];
-let speeds = [];
-let languagess = [];
-let visions = [];
-let traitss = [];
-let document = [];
 
-async function getData(){
-    try{
-        const response = await fetch(`https://api.open5e.com/v1/races/`);
 
-        if (response.status != 200){
-            throw new Error(response);
-        } else {
-            const data = await response.json();
-            console.log(data.results);
-        }
-    } catch(error) {
-        console.log(error);
-}}
 
-getData();
+try {
+    const response = await fetch(`https://api.open5e.com/v1/races/`);
 
-/*
+    if (response.status != 200){
+        throw new Error(response);
+    } else {
+        const data = await response.json();
+        const races = data.results;
 
-data.results.forEach(i => {
-                    names.push(i.name);
-                    descs.push(i.desc);
-                    asis.push(i.asi_desc);
-                    ages.push(i.age);
-                    alignments.push(i.alignment);
-                    sizes.push(i.size);
-                    speeds.push(i.speed_desc);
-                    languagess.push(i.languages);
-                    document.push(i.document_title);
+        races.forEach(r => {
+                document.querySelector(".stuff")
+                    .insertAdjacentHTML("beforeend",`
+                        <div></div>
+                    `)
+        })
+    }
+} catch(error) {
+    console.log(error);
+}
 
-                    if (i.vision === ""){
-                        visions.push("This race has regular vision.");
-                    } else {
-                        visions.push(i.vision);
-                    }
-                    
-                    if (i.traits === ""){
-                        traitss.push("This race has no special traits");
-                    } else {
-                        traitss.push(i.traits);
-                    }
-                }); */
+
+
