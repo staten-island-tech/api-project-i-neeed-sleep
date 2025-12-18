91ca323cb0fc31187257142521d57a9cb26db758
i-neeed-sleep <kammyl@nycstudents.net>
2025-12-18 14:02:26 -0500

    eeeeeeeeeeeeeeee

--- dnd/src/main.js
+++ dnd/src/main.js
@@ -1,4 +1,3 @@
-
 async function page() {
     try {
         const response = await fetch(`https://api.open5e.com/v1/races/`);
@@ -8,15 +7,14 @@ async function page() {
         } else {
             const data = await response.json();
             const races = data.results;
-
             races.forEach(r => {
                 document.querySelector("#stuff")
                     .insertAdjacentHTML("beforeend",`
-                        <div class = "card">
-                            <h2 id="name">${r.name}</h2>
-                            <h2 id="desc">${r.asi_desc}</h2>
-                            <h2 id="desc">${r.alignment}</h2>
-                            <a href="${r.document_url}"><h2 id="doc">This race is from ${r.document_title}</h2></a>  
+                        <div id = "card" class="">
+                            <div id="name">${r.name}</div>
+                            <div id="desc">${r.asi_desc}</div>
+                            <div id="desc">${r.alignment}</div>
+                            <a href="${r.document_url}"><div id="doc">This race is from ${r.document_title}</div></a>  
                             <button id = "details">More Details</button>
                         </div>
                         <br>
@@ -28,15 +26,27 @@ async function page() {
                     let n = s.querySelector("#name").value;
                     let race = races.filter(race => race.name == n);
                     document.querySelector("#stuff").innerHTML(`
-                        <h1>${n}</h1>
-                        <h2>${race.asi_desc}</h2>
-                        <h2 id="desc">${r.alignment}</h2>
-                        <h2>${race.traits}</h2>
-                        <h2>${race.age}</h2>
-                        <h2>${race.size}</h2>
-                        <h2>${race.speed_desc}</h2>
-                        <h2>${race.languages}</h2>
-                        <h2>${race.vision}</h2>
+                        <div>
+                            <div>${n}</div>
+                            <br>
+                            ${race.asi_desc}
+                            <br>
+                            ${r.alignment}
+                            <br>
+                            ${race.traits}
+                            <br>
+                            ${race.age}
+                            <br>
+                            ${race.size}
+                            <br>
+                            ${race.speed_desc}
+                            <br>
+                            ${race.languages}
+                            <br>
+                            ${race.vision}
+                            <br>
+                        </div>
+                        
                     `)
                 })
             });
