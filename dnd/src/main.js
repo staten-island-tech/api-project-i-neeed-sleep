7d699c4fb1c44091efbe5ec87841473324a2cb10
i-neeed-sleep <kammyl@nycstudents.net>
2026-01-07 13:52:04 -0500

    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- dnd/src/main.js
+++ dnd/src/main.js
@@ -13,53 +13,54 @@ async function page() {
             races.forEach(r => {
                 document.querySelector("#stuff")
                     .insertAdjacentHTML("beforeend",`
-                        <div id = "card" class="font-serif box-border w-7/10 l-150 border-3 text-yellow-900 justify-self-center bg-sky-100">
-                            <div id="name" class="font-bold">${r.name}</div>
-                            <div id="desc" class="">${r.asi_desc}</div>
-                            <div id="desc" class="">${r.alignment}</div>
-                            <a href="${r.document__url}"><div id="doc" class="underlines">This race is from ${r.document__title}</div></a>  
-                            <button class="bg-rose-950 hover:bg-rose-900 text-yellow-900 font-bold py-2 px-4 rounded-full duration-300 ease-out" id = "details">More Details</button>
+                        <div id = "card" name = "${r.name}" class="font-serif box-border w-7/10 l-150 border-4 text-yellow-900 justify-self-center bg-sky-100 p-8 m-10">
+                            <div class="font-bold text-2xl">${r.name}</div>
+                            <div class="text-lg">
+                                <div>${r.alignment}</div>
+                                <br>
+                                <div class="font-bold">Ability Score Increase</div>
+                                <div>${r.asi_desc}</div>
+                                <br>
+                                <div class="font-bold">Age</div>
+                                <div>${r.age}</div>
+                                <br>
+                                <div class="font-bold">Size</div>
+                                <div>${r.size}</div>
+                                <br>
+                                <div class="font-bold">Speed</div>
+                                <div>${r.speed_desc}</div>
+                                <br>
+                                <div class="font-bold">Language</div>
+                                <div>${r.languages}</div>
+                                <br>
+                                <div class="font-bold">Vision</div>
+                                <div>${r.vision}</div>
+                                <br>
+                                <div class="font-bold">Racial Traits</div>
+                                <div>${r.traits}</div>
+                            </div>
+                            
+                            <br>
+                            <a href="${r.document__url}"><div class="underline">This race is from ${r.document__title}</div></a> 
                         </div>
-                        <br>
                     `)
             })
 
-            document.querySelectorAll("#details").
-                forEach(i => {i.addEventListener("click", s => {
-                    let n = s.target.closest("#name").value;
-                    let race = races.filter(race => race.name == n);
-
-                    document.querySelector("#stuff").innerHTML = 
-                        `<div class="font-serif">
-                            <button class="py-2 px-4 rounded-full">EXIT DETAILS</button>
-                            <h1>${n}</h1>
-                            <h3>${race.asi_desc}</h3>
-                            <br>
-                            <h2>ALignment</h2>
-                            <h3>${race.alignment}</h3>
-                            <br>
-                            <h2>Special Traits</h2>
-                            <h3>${race.traits}</h3>
-                            <br>
-                            <h2>Lifespan</h2>
-                            <h3>${race.age}</h3>
-                            <br>
-                            <h2>Size</h2>
-                            <h3>${race.size}</h3>
-                            <br>
-                            <h2>Walkspeed</h2>
-                            <h3>${race.speed_desc}</h3>
-                            <br>
-                            <h2>Linguistics</h2>
-                            <h3>${race.languages}</h3>
-                            <br>
-                            <h2>Vision</h2>
-                            <h3>${race.vision}</h3>
-                            <br>
-                        </div>`;
+            function fltr(ask){
+                document.querySelectorAll("#card").forEach(card => {
+                    if(card.getAttribute("name").toLowerCase().includes(ask)){
+                        card.style.display = "block";
+                    }else{
+                        card.style.display = "none";
+                    }
                 })
-            });
+            }
 
+            document.querySelector('#search').addEventListener('submit', ask =>{
+                ask.preventDefault();
+                let input = document.querySelector('#srch').value.toLowerCase();
+                fltr(input);
+            })
         }
 
     } catch(error) {
